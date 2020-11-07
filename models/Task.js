1821470bd67fbe1f2e7941bdf74693ddf30f0a1f
Ivan Volodartsev <untrupus@gmail.com>
2020-11-07 22:24:40 +0600

    create middleware and task model

---
+++ models/Task.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const TaskSchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: String,
+    status: {
+        type: String,
+        enum: ["new", "in_progress", "done"],
+        default: "new"
+    }
+});
+
+const Task = mongoose.model("Task", TaskSchema);
+
+module.exports = Task;
